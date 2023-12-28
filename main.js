2920783c2dbb14f7ea658b87e52f334abc3f47d0
Emmanuel1287 <emmaitams@gmail.com>
2023-12-28 22:14:38 +0100

    first commit

--- main.js
+++ main.js
@@ -10,3 +10,27 @@ var swiper = new Swiper(".home", {
     clickable: true,
   },
 });
+
+var swiper = new Swiper(".coming-container", {
+  spaceBetween: 20,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 55000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 2,
+    },
+    568: {
+      slidesPerview: 3,
+    },
+    768: {
+      slidesPerView: 4,
+    },
+    968: {
+      slidesPerView: 5,
+    },
+  },
+});
